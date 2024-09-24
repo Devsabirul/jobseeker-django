59240c5ab2e5bff28a972cf256d5ef6c257e9846
touhidul-islamm <rony.touhidul@gmail.com>
2024-09-24 19:32:09 +0600

    view problem

---
+++ u_account/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+from .models import *
+
+class SignupForm(UserCreationForm):
+    
+    class Meta:
+        model= User
+        fields= ['first_name','username','email','password1']
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model= Profile
+        fields=['phone']
--- u_account/views.py
+++ u_account/views.py
@@ -1,9 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import *
 
 # Create your views here.
 def signup(request):
+    if request.method=='POST':
+        s_form=SignupForm(request.POST, request.FILES, instance=request.user)
+        p_form=ProfileForm(request.POST, request.FILES, instace=request.user.Profile)
+        if s_form.is_valid() and p_form.is_valid():
+            s_form.save()
+            p_form.save()
+            return redirect('indexpage')
+    else:
+        s_form=SignupForm(request.POST, instance=request.user)
+        p_form=ProfileForm(request.POST, instace=request.user.Profile)
 
-    return render(request, 'signup.html')
+    context={
+        's_form':s_form,
+        'p_form':p_form
+    }
+
+    return render(request,'userapp/signup.html', context)
 
 def login(request):
     
