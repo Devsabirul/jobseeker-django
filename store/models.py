0a1c6244b99dfed2cef9179c45a8a639207ddc2b
Shishir Islam Shanto <mdsabirulislam8089@gmail.com>
2024-10-03 00:15:21 +0600

    Job details model change

--- store/models.py
+++ store/models.py
@@ -20,7 +20,7 @@ class Job_details(models.Model):
     profile=models.ForeignKey(Profile, on_delete=models.CASCADE)
     title=models.CharField(max_length=100)
     job_description=models.TextField(max_length=200, blank=True, null=True)
-    work_deadline=models.DateTimeField(auto_now_add=True)
+    work_deadline=models.DateTimeField(auto_now_add=False)
     status=models.BooleanField(default=True)
     job_location=models.CharField(max_length=100, blank=True, null=True)
     area = models.CharField(max_length=100,default="UK")
--- store/views.py
+++ store/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from .models import *
+from django.db.models import Q
+
 def index(request):
-    jobs = Job_details.objects.all()
+    jobs = Job_details.objects.order_by("-id")
     categorys = Categories.objects.all()
     areas = Area.objects.all()
     return render(request, 'index.html',locals())
@@ -34,7 +36,8 @@ def search(request):
     title = request.GET.get("search")
     area = request.GET.get("area")
     category = request.GET.get("category")
-
+    jobs = Job_details.objects.filter(Q(title=title) | Q(area=area)|Q(category=category))
+    print(jobs)
     print(title,area,category)
     return render(request, 'search.html')
 
